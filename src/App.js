a92773643a5c8ba6f6ea3d3e2916602437525ccb
SlavikChuprin <chuprin.00@yandex.ru>
2021-11-14 12:39:31 +0300

    add useState hook

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 import Section from './components/Section';
 import FeedbackOptions from './components/FeedbackOptions';
 import Statistics from './components/Statistics';
 import Notification from './components/Notification';
 
-export default class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  handleIncrement = e => {
+  const handleIncrement = e => {
     const btnName = e.currentTarget.outerText;
+    switch (btnName) {
+      case good:
+        setGood(state => state + 1);
+        break;
 
+      case neutral:
+        setNeutral(state => state + 1);
+        break;
+
+      case bad:
+        setBad(state => state + 1);
+        break;
+
+      default:
+        console.log('нет такой оценки');
+    }
     this.setState(prevState => ({
       [btnName]: prevState[btnName] + 1,
     }));
@@ -25,29 +38,28 @@ export default class App extends Component {
 
   positivePercentage = state =>
     Math.round((this.state.good / this.totalScore()) * 100);
-  render() {
-    const nameButton = Object.keys(this.state);
-    return (
-      <div className="App">
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={nameButton}
-            onLeaveFeedback={this.handleIncrement}
+
+  const nameButton = Object.keys(this.state);
+  return (
+    <div className="App">
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={nameButton}
+          onLeaveFeedback={this.handleIncrement}
+        />
+      </Section>
+
+      {this.totalScore(this.state) === 0 ? (
+        <Notification message="No feedback given..." />
+      ) : (
+        <Section title="Statistics">
+          <Statistics
+            options={this.state}
+            onTotal={this.totalScore}
+            onPositivePercentage={this.positivePercentage}
           />
         </Section>
-
-        {this.totalScore(this.state) === 0 ? (
-          <Notification message="No feedback given..." />
-        ) : (
-          <Section title="Statistics">
-            <Statistics
-              options={this.state}
-              onTotal={this.totalScore}
-              onPositivePercentage={this.positivePercentage}
-            />
-          </Section>
-        )}
-      </div>
-    );
-  }
+      )}
+    </div>
+  );
 }
